baf41b1df96a0addeb25aefb21b68172a8cb131e
Ramiro Castillo <rcastilloc@falabella.cl>
2022-11-16 18:03:26 -0300

    Changed hardcoded path for kubeconfig

--- cluster-services/namespaces/providers.tf
+++ cluster-services/namespaces/providers.tf
@@ -1,4 +1,4 @@
 provider "kubernetes" {
-  config_path = "/Users/ramirocastillo/Desktop/mentoring/terraforming_gcp/gcp/gke-deployment/kubeconfig-dev"
+  config_path = "${var.kubeconfig_path}"
   #   config_context = "my-context"
 }
--- cluster-services/namespaces/variables.tf
+++ cluster-services/namespaces/variables.tf
@@ -6,3 +6,8 @@ variable "ns_names" {
     })
   )
 }
+
+variable "kubeconfig_path" {
+    type = string
+    description = "Kubeconfig path defined as 'TF_VAR_kubeconfig_path'"
+}
